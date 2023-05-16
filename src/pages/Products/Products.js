5f2f1123b9ec705c06f329c882107ab562d07c0b
mohammad <mo.se.12369@gmail.com>
2023-05-16 21:18:36 +0330

    #

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React from 'react'
+import Card from '../../components/Card'
+import ProductsList from '../../data/ProductsList'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
-export default function Products(){
-    return(
-        <main className="main">
-            <h1>Products</h1>
-        </main>
+export default function Products(props) {
+  const cards = ProductsList.map((item) => {
+    return (
+      <Card key={item.id} id={item.id} item={item} darkMode={props.darkMode} />
     )
+  })
+  return (
+    <main className="main">
+      <h1>Products</h1>
+      <div className="row px-5 mb-5 gy-2 gx-2 justify-content-evenly cards--container">
+        {cards}
+      </div>
+    </main>
+  )
 }
