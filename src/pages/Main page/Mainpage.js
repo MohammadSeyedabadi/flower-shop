65f09e68f9047e147589c1ea062c6dcfa3000122
mohammad <mo.se.12369@gmail.com>
2023-05-27 21:37:51 +0330

    added image slider

--- src/pages/Main page/Mainpage.js
+++ src/pages/Main page/Mainpage.js
@@ -3,11 +3,70 @@ import Card from '../../components/Card'
 import ProductsList from '../../data/ProductsList'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import Picture from '../../images/p11.jpg'
+import P2 from "../../images/p2.jpg"
+import P3 from "../../images/p3.jpg"
+import P4 from "../../images/p4.jpg"
+import P5 from "../../images/p5.jpg"
+import P6 from "../../images/p6.jpg"
 
 export default function Mainpage(props) {
   const cards = ProductsList.slice(0, 8).map((item) => {
     return <Card key={item.id} id={item.id} item={item} darkMode={props.darkMode} />
   })
+
+  const Thumbnail = ({ arr, image, index }) => {
+    return (
+      <div className="thumbnail">
+        {arr.map((imgsrc, i) => (
+          <img
+            key={i}
+            src={imgsrc}
+            onClick={() => image(i)}
+            className={index === i ? 'cursor-style thumbnail--img active' : 'thumbnail--img cursor-style'}
+          />
+        ))}
+      </div>
+    )
+  }
+
+  const Slideshow = ({ imgs }) => {
+    const [index, setIndex] = React.useState(0)
+
+    React.useEffect(() => {
+      setIndex(0)
+    }, [])
+    const next = () => {
+      if (index === imgs.length - 1) {
+        setIndex(0)
+      } else {
+        setIndex(index + 1)
+      }
+    }
+    const prev = () => {
+      if (index === 0) {
+        setIndex(imgs.length - 1)
+      } else {
+        setIndex(index - 1)
+      }
+    }
+
+    return (
+      <div className="slideshow">
+        <img className="img-fluid mainImg" src={imgs[index]} />
+        <div className="actions">
+          <button onClick={prev}>
+            <i className="bi bi-chevron-left"></i>
+          </button>
+          <button onClick={next}>
+            <i className="bi bi-chevron-right"></i>
+          </button>
+        </div>
+        <Thumbnail arr={imgs} image={setIndex} index={index} />
+      </div>
+    )
+  }
+
+
   return (
     <main
       className={
@@ -109,6 +168,36 @@ export default function Mainpage(props) {
       <div className="row px-5 mb-5 gy-2 gx-2 justify-content-evenly cards--container">
         {cards}
       </div>
+
+      <div className="row pt-5 mb-5 d-flex align-items-center">
+        <div className="col-md-6 slider--container">
+          <Slideshow
+            imgs={[
+              P2,
+              P3,
+              P4,
+              P5,
+              P6,
+            ]}
+          />
+        </div>
+        <div className="col-md-6">
+          <h1
+            className={
+              props.darkMode
+                ? 'mt-3 letter-spacing'
+                : 'mt-3 letter-spacing light--text-color'
+            }
+          >
+            Our clients prefer
+          </h1>
+          <p className={props.darkMode ? 'fs-4 dark--text-color' : 'fs-4'}>
+            Lorem ipsum dolor sit amet summ dolore, eu omnes mnesarchum
+            eosatsimuscum primis.
+          </p>
+        </div>
+      </div>
+
     </main>
   )
 }
