b4ed01a281de20c2be50a5ec442c80a6fea1eab0
mohammad <mmd.se.7@gmail.com>
2023-06-07 21:07:16 +0330

    #

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Container from 'react-bootstrap/Container'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Mainpage from './pages/Main page/Mainpage'
-import Products from "./pages/Products/Products"
-import Cart from "./pages/Cart/Cart"
+import Products from './pages/Products/Products'
+import Cart from './pages/Cart/Cart'
 import Footer from './components/Footer/Footer'
-import { ShopContextProvider } from "./context/shop-context";
+import { ShopContextProvider } from './context/shop-context'
 
 export default function App() {
   const [darkMode, setDarkMode] = React.useState(false)
@@ -28,14 +28,15 @@ export default function App() {
     <div className={darkMode ? 'dark' : 'light'}>
       <Container fluid className="app--container">
         <ShopContextProvider>
-        <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-        {/* <Routes> */}
-        {/* <Route path="/my-website" element={<Mainpage darkMode={darkMode} />} /> */}
-        <Routes>
-          <Route path="/" element={<Mainpage darkMode={darkMode} />} />
-          <Route path="/products" element={<Products darkMode={darkMode} />} />
-          <Route path="/cart" element={<Cart darkMode={darkMode} />} />
-        </Routes>
+          <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+          <Routes>
+            <Route path="/" element={<Mainpage darkMode={darkMode} />} />
+            <Route
+              path="/products"
+              element={<Products darkMode={darkMode} />}
+            />
+            <Route path="/cart" element={<Cart darkMode={darkMode} />} />
+          </Routes>
         </ShopContextProvider>
         <Footer darkMode={darkMode} />
       </Container>
