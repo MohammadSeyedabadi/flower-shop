01ecd6acda9acbd4b390c5191786bf896b46ce5f
mohammad <mmd.se.7@gmail.com>
2023-06-07 20:13:03 +0330

    updated form section

---
+++ src/pages/Main page/Form/Form.js
@@ -0,0 +1,95 @@
+import React from "react";
+import Col from 'react-bootstrap/Col'
+
+export default function Form({darkMode}){
+
+    const [status, setStatus] = React.useState('Submit')
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setStatus('Sending...')
+    const { email, message } = e.target.elements
+    let details = {
+      email: email.value,
+      message: message.value,
+    }
+    let response = await fetch('http://localhost:5000/contact', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify(details),
+    })
+    setStatus('Submit')
+    let result = await response.json()
+    alert(result.status)
+  }
+
+  
+    return(
+        <Col lg={6}>
+          <div
+            className={`p-5 text-center h-100 ${
+              darkMode ? 'card--stats-dark' : 'localflorist'
+            }`}
+          >
+            <h1
+              className={`letter-spacing ${
+                darkMode ? '' : 'light--text-color'
+              }`}
+            >
+              Ask us a question
+            </h1>
+            <p
+              className={`mt-4 fs-4 px-lg-5 ${
+                darkMode ? 'dark--text-color' : ''
+              }`}
+            >
+              Lorem ipsum dolor sit amet, pri autem nemore bonorum te. Autem
+              fierent ullamcorper ius no.
+            </p>
+            <div>
+              <label
+                htmlFor="email"
+                className={`fw-semibold ${
+                  darkMode ? 'form--label-dark' : 'form--label'
+                }`}
+              ></label>
+              <input
+                type="email"
+                id="email"
+                className={`fw-semibold p-1 mb-3 w-100 ${
+                  darkMode ? 'form--input-dark' : 'form--input'
+                }`}
+                placeholder="Your Email"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="message"
+                className={`fw-semibold ${
+                  darkMode ? 'form--label-dark' : 'form--label'
+                }`}
+              ></label>
+              <textarea
+                id="message"
+                rows={4}
+                className={`fw-semibold p-1 mb-3 w-100  ${
+                  darkMode ? 'form--input-dark' : 'form--input'
+                }`}
+                placeholder="Your message"
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className={`d-flex form--button ${
+                darkMode ? 'form--button-dark' : ''
+              }`}
+            >
+              {status}
+            </button>
+          </div>
+        </Col>
+    )
+}
--- src/pages/Main page/Mainpage.js
+++ src/pages/Main page/Mainpage.js
@@ -4,6 +4,7 @@ import Services from './Services/Services'
 import ServicesList from './Services/ServicesList'
 import FindUs from './Find us/FindUs'
 import PlacesList from './Find us/PlacesList'
+import Form from "./Form/Form"
 //import { Link } from 'react-router-dom'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -35,28 +36,7 @@ export default function Mainpage({ darkMode }) {
   const findus = PlacesList.map((item) => {
     return <FindUs key={item.id} id={item.id} item={item} darkMode={darkMode} />
   })
-
-  const [status, setStatus] = React.useState('Submit')
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    setStatus('Sending...')
-    const { email, message } = e.target.elements
-    let details = {
-      email: email.value,
-      message: message.value,
-    }
-    let response = await fetch('http://localhost:5000/contact', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-      },
-      body: JSON.stringify(details),
-    })
-    setStatus('Submit')
-    let result = await response.json()
-    alert(result.status)
-  }
-
+  
   return (
     <main className={`main ${darkMode ? 'dark' : ''}`}>
       <Row className="mb-5 d-flex align-items-center">
@@ -251,71 +231,7 @@ export default function Mainpage({ darkMode }) {
             </p>
           </div>
         </Col>
-        <Col lg={6}>
-          <div
-            className={`p-5 text-center h-100 ${
-              darkMode ? 'card--stats-dark' : 'localflorist'
-            }`}
-          >
-            <h1
-              className={`letter-spacing ${
-                darkMode ? '' : 'light--text-color'
-              }`}
-            >
-              Ask us a question
-            </h1>
-            <p
-              className={`mt-4 fs-4 px-lg-5 ${
-                darkMode ? 'dark--text-color' : ''
-              }`}
-            >
-              Lorem ipsum dolor sit amet, pri autem nemore bonorum te. Autem
-              fierent ullamcorper ius no.
-            </p>
-            <div>
-              <label
-                htmlFor="email"
-                className={`fw-semibold ${
-                  darkMode ? 'form--label-dark' : 'form--label'
-                }`}
-              ></label>
-              <input
-                type="email"
-                id="email"
-                className={`fw-semibold p-1 mb-3 w-100 ${
-                  darkMode ? 'form--input-dark' : 'form--input'
-                }`}
-                placeholder="Your Email"
-                required
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="message"
-                className={`fw-semibold ${
-                  darkMode ? 'form--label-dark' : 'form--label'
-                }`}
-              ></label>
-              <textarea
-                id="message"
-                rows={4}
-                className={`fw-semibold p-1 mb-3 w-100  ${
-                  darkMode ? 'form--input-dark' : 'form--input'
-                }`}
-                placeholder="Your message"
-                required
-              />
-            </div>
-            <button
-              type="submit"
-              className={`d-flex form--button ${
-                darkMode ? 'form--button-dark' : ''
-              }`}
-            >
-              {status}
-            </button>
-          </div>
-        </Col>
+        <Form darkMode={darkMode}/>
       </Row>
     </main>
   )
