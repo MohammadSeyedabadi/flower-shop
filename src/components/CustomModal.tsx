92950b981230780d82415b3f1d9b5016b747117c
unknown <mhmd.sey.7@gmail.com>
2023-11-29 20:55:25 +0330

    #

--- src/components/CustomModal.tsx
+++ src/components/CustomModal.tsx
@@ -1,22 +1,40 @@
 import { useState, useRef } from "react";
 
 export default function CustomModal() {
-    // const myDialog = useRef<any>();
-    // function handleClick(){
-    //     myDialog.current.showModal();
-    // }
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  function detectOutSideClick(event: any) {
+    // console.log(dialogRef.current.contains(event.target) , "LOOk THIS")
+    if (dialogRef.current) {
+      const dialogDimensions = dialogRef.current.getBoundingClientRect();
+      if (
+        event.clientX < dialogDimensions.left ||
+        event.clientX > dialogDimensions.right ||
+        event.clientY < dialogDimensions.top ||
+        event.clientY > dialogDimensions.bottom
+      ) {
+        dialogRef.current.close();
+      }
+    }
+  }
+
   return (
     <>
       <div className="row">
         <div className="column">
-          <dialog>hello</dialog>
+          <dialog
+            ref={dialogRef}
+            className="m-auto"
+            onClick={(event) => detectOutSideClick(event)}
+          >
+            <img src="/flowerImages/p5.jpg" className="modal--image" />
+          </dialog>
+          <div onClick={() => dialogRef.current?.showModal()}><img src="/flowerImages/p5.jpg" /></div>
         </div>
-       
         <div className="column"></div>
         <div className="column"></div>
         <div className="column"></div>
       </div>
-
     </>
   );
 }
